6f5c2cb80265676b1fd8da8e85151eb92272ccc8
BlackSarkk <droncer22446688@gmail.com>
2025-12-01 01:48:29 +0530

    Do W9 PPA-1

--- W9_binarySearch_recursion/PPA/PPA1.py
+++ W9_binarySearch_recursion/PPA/PPA1.py
@@ -1,3 +1,12 @@
 
 # Write a recursive function named triangular that accepts a positive integer n as argument and returns the sum of the first n positive integers.
 # You do not have to accept input from the user or print the output to the console. You just have to write the function definition.
+
+def triangular(n):
+    if n == 1:
+        return 1
+    else:
+        return n + triangular(n-1)
+    
+
+print(triangular(4))
