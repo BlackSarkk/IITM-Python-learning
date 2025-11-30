ca2b146dd394cda6016575513e54efa05ef02cb6
BlackSarkk <droncer22446688@gmail.com>
2025-12-01 01:50:55 +0530

    Do W9 PPA-2

--- W9_binarySearch_recursion/PPA/PPA2.py
+++ W9_binarySearch_recursion/PPA/PPA2.py
@@ -3,3 +3,12 @@
 # n!=1x2x3⋯n
 # Write a recursive function named factorial that accepts a positive integer n as argument and returns the factorial of n.
 # You do not have to accept input from the user or print the output to the console. You just have to write the function definition.
+
+
+def factorial(n):
+    if n<=1:
+        return 1
+    else:
+        return n*factorial(n-1)
+    
+print(factorial(1))
