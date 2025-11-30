23f4a6104aaa0efd60ec9f93e221512cfb2df4a0
BlackSarkk <droncer22446688@gmail.com>
2025-12-01 03:16:16 +0530

    Do W9 PPA-7

--- W9_binarySearch_recursion/PPA/PPA7.py
+++ W9_binarySearch_recursion/PPA/PPA7.py
@@ -12,3 +12,21 @@ This function should return the number of occurrences of word in L.
 (3) You do not have to accept input from the user or print the output to the console. You just have to write the definition of both the functions.
 
 '''
+
+
+def count(L, word):
+
+    if (len(L)==1):
+        if L[0] == word:
+            return 1
+        else:
+            return 0
+    
+    elif (L[0] != word):
+        return count(L[1:], word)
+    
+    else:
+        return 1 + count(L[1:], word)
+    
+
+print(count(["do", "re", "si", 'mi', "fa", "si"], "si"))
