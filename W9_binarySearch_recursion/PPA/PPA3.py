aa56019898e7ea16cc2bb3a0bb7d5fba06449f50
BlackSarkk <droncer22446688@gmail.com>
2025-12-01 02:21:29 +0530

    Do W9 PPA-3

--- W9_binarySearch_recursion/PPA/PPA3.py
+++ W9_binarySearch_recursion/PPA/PPA3.py
@@ -2,3 +2,16 @@
 
 # Write a recursive function named multiply accepts two positive integers a and b as argument and returns their product. You can only use + and − operators. You are not allowed to use the ∗ symbol anywhere in your code!
 # You do not have to accept input from the user or print the output to the console. You just have to write the function definition.
+
+
+
+def multiply(a, b):
+    if a==0 or b==0:
+        return 0
+    if b == 1:
+        return a
+    else:
+        return a + multiply(a, b-1)
+    
+
+print(multiply(9, 3))
