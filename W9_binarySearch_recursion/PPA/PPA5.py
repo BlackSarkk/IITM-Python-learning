9fa6ee201258337804debc765caaae7ed44d6237
BlackSarkk <droncer22446688@gmail.com>
2025-12-01 03:04:09 +0530

    Do W9 PPA-5

--- W9_binarySearch_recursion/PPA/PPA5.py
+++ W9_binarySearch_recursion/PPA/PPA5.py
@@ -4,3 +4,17 @@ Write a recursive function named palindrome that accepts a string word as argume
 You do not have to accept input from the user or print the output to the console. You just have to write the function definition.
 '''
 
+def palindrome(string):
+
+    if len(string) <= 1:
+        return True
+    
+    elif string[0] == string[-1]:
+        return palindrome(string[1:-1]) 
+    else:
+        return False 
+    
+print(palindrome("wowa"))
+
+
+# NOTE: Here order or validations matter
