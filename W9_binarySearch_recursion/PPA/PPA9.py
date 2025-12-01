ccb52d8a7155ef56442c41ab8643a74efe87a62f
BlackSarkk <droncer22446688@gmail.com>
2025-12-01 14:40:40 +0530

    Do W9 PPA-9

--- W9_binarySearch_recursion/PPA/PPA9.py
+++ W9_binarySearch_recursion/PPA/PPA9.py
@@ -4,3 +4,17 @@
 Write a recursive function named uniq that accepts a non-empty list L as argument and returns a new list after removing all duplicates from it. Your function must retain the last occurrence of each distinct element in the list.
 You do not have to accept input from the user or print the output to the console. You just have to write the function definition.
 '''
+
+
+# def uniq(L):
+#     if len(L) <= 1:
+#         return L
+    
+#     rest = uniq(L[1:])
+
+#     if L[0] in rest:
+#         return rest
+#     else:
+#         return rest + [L[0]]          # .append() wont work here" as append will return none
+    
+
