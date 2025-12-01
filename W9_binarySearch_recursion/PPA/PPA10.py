e064bdc81c758241b2b3644258898cc50047824b
BlackSarkk <droncer22446688@gmail.com>
2025-12-01 15:08:47 +0530

    Do W9 PPA-10

--- W9_binarySearch_recursion/PPA/PPA10.py
+++ W9_binarySearch_recursion/PPA/PPA10.py
@@ -8,3 +8,21 @@ The function should return True if k is found in the list L, and False otherwise
 You do not have to accept input from the user or print output to the console. You just have to write the function definition.
 
 '''
+
+
+def search(L, k):
+
+    if len(L) == 0:
+        return False
+
+    mid = len(L)//2
+
+    if L[mid]==k:
+        return True
+    elif L[mid] > k:
+        return search(L[:mid], k)
+    elif L[mid] < k:
+        return search(L[mid+1:], k)
+
+
+print(search([1, 2, 3, 4, 5, 6, 7], 5))
